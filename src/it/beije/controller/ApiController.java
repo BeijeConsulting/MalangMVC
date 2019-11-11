ff4299d37e466fae924e34c67e82e8ac7e337893
vittorioluoni <vittorio.luoni@libero.it>
2019-11-11 14:33:48 +0100

    aggiornamento malangMVC

--- src/it/beije/controller/ApiController.java
+++ src/it/beije/controller/ApiController.java
@@ -1,12 +1,19 @@
 package it.beije.controller;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import it.beije.controller.bean.Contatto;
 
 
@@ -26,9 +33,34 @@ public class ApiController {
 	public @ResponseBody Contatto testpost(@RequestBody Contatto contatto) {
 		System.out.println("TEST POST");
 		
-		contatto.setId(100);
-
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("MalangMVC");
+		EntityManager entityManager = factory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+		entityManager.persist(contatto);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		return contatto;
+	}
+	
+	@RequestMapping(value = "/getcontatto", method = RequestMethod.POST)
+	public @ResponseBody Contatto getcontatto(@RequestParam("id") int n) {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("MalangMVC");
+		EntityManager entityManager = factory.createEntityManager();
+		Contatto contatto=entityManager.find(Contatto.class, n);
 		return contatto;
 	}
 
+	@RequestMapping(value = "/getcontatti", method = RequestMethod.GET)
+	public @ResponseBody List<Contatto> getcontatti() {
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("MalangMVC");
+		EntityManager entityManager = factory.createEntityManager();
+		TypedQuery<Contatto> query=entityManager.createQuery("SELECT c FROM Contatto c",Contatto.class);
+		List<Contatto> contatti=query.getResultList();
+		return contatti;
+	}
+
+		
 }
